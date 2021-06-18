569cf08420cfeeec7be7fdeb86c82502f7382cae
digitalronin <david@digitalronin.com>
2021-06-18 16:28:17 +0900

    Fetch and display the string from the contract

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import Greeter from './abi/Greeter.json'
+
+const greeterAddress = `${process.env.REACT_APP_GREETER_CONTRACT_ADDRESS}`;
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  // get the current greeting value from the contract
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+        setGreetingValue(data);
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>{greeting}</h3>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
       </header>
     </div>
   );
