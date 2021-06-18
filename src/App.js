7df893188bd3bbf358ddc2ea034ce06ffa3127cf
digitalronin <david@digitalronin.com>
2021-06-18 16:39:50 +0900

    Enable user to update the contract greeting string

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const greeterAddress = `${process.env.REACT_APP_GREETER_CONTRACT_ADDRESS}`;
 function App() {
   const [greeting, setGreetingValue] = useState();
 
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
   // get the current greeting value from the contract
   async function fetchGreeting() {
     if (typeof window.ethereum !== 'undefined') {
@@ -23,11 +28,28 @@ function App() {
     }
   }
 
+  // Call the contract to set a new greeting string
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>{greeting}</h3>
         <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <br />
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="New greeting" />
+        <button onClick={setGreeting}>Set Greeting</button>
       </header>
     </div>
   );
